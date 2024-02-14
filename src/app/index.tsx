42a2116324a8bfd5ec2dc1e7a6bd19f028d8989c
LucasFerdal <electroblue25@gmail.com>
2024-02-13 21:39:24 -0300

    added: firebase connection

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import Auth from '../components/auth';
 
 import { MotiView } from 'moti';
 
 export default function Page() {
   return (
-    <MotiView
-      className="flex flex-1 items-center justify-center"
-      from={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ type: 'timing', duration: 3000 }}
-    >
-      <Text className="text-4xl font-bold">Hola</Text>
-    </MotiView>
+      <View>
+        <Auth />
+
+      </View>
   );
 }
