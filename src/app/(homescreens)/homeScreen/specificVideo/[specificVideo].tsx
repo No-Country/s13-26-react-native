4a60df4b399e54a3ea30b32a31035fb3b48940fe
fanofrisoni <fanofrisoni4@gmail.com>
2024-02-27 12:20:02 -0600

    Styles specific video

--- src/app/(homescreens)/homeScreen/specificVideo/[specificVideo].tsx
+++ src/app/(homescreens)/homeScreen/specificVideo/[specificVideo].tsx
@@ -8,7 +8,16 @@ function SpecificVideo() {
   return (
     <View style={style.container}>
       <View style={style.containerTitle}>
-        <Text> {specificVideo.titulo}</Text>
+        <Text
+          style={{
+            fontFamily: 'montserrat_semibold',
+            fontSize: 18,
+            textAlign: 'center',
+          }}
+        >
+          {' '}
+          {specificVideo.titulo}
+        </Text>
       </View>
       <View style={style.containerImg}>
         <Image
@@ -17,59 +26,66 @@ function SpecificVideo() {
             uri: specificVideo.url,
           }}
         />
-        <MaterialCommunityIcons
-          name="clock-time-five-outline"
-          size={24}
-          color="black" />
-        <Text>{specificVideo.duracion}</Text>
+        <View style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', gap: 4 }}>
+          <MaterialCommunityIcons name="clock-time-five-outline" size={22} color="black" />
+          <Text style={{ fontFamily: 'montserrat_regular', fontSize: 16 }}>
+            {specificVideo.duracion} s
+          </Text>
+        </View>
       </View>
       <View style={style.containerDescription}>
-        <Text>{specificVideo.descripcion}</Text>
+        <Text
+          style={{
+            fontFamily: 'montserrat_medium',
+            fontSize: 14,
+            textAlign: 'center',
+            lineHeight: 26,
+            paddingHorizontal: 6,
+          }}
+        >
+          {specificVideo.descripcion}
+        </Text>
       </View>
-      <TouchableOpacity style={style.containerStopwatch} >
-        <MaterialCommunityIcons
-          name="clock-time-five-outline"
-          size={100}
-          color="black" />
-        <View
-          style={style.controllerPlay}>
+      <TouchableOpacity style={style.containerStopwatch}>
+        <MaterialCommunityIcons name="clock-time-five-outline" size={100} color="black" />
+        <View style={style.controllerPlay}>
           <Entypo name="controller-play" size={24} color="black" />
         </View>
       </TouchableOpacity>
-
     </View>
   );
 }
 export default SpecificVideo;
 const style = StyleSheet.create({
   container: {
+    paddingVertical: 21,
+    paddingHorizontal: 16,
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    display: 'flex',
+    marginBottom: 10,
   },
   containerTitle: {
-    marginBottom: 15,
+    marginBottom: 6,
   },
   containerImg: {
-    alignItems: "center",
-    marginBottom: 15,
+    alignItems: 'center',
   },
   img: {
-    width: 100,
-    height: 100,
-    marginBottom: 5,
-
+    width: 220,
+    height: 220,
+    marginBottom: 8,
+    borderRadius: 10,
   },
-  containerDescription: {
-    marginBottom: 15,
-  },
+  containerDescription: {},
   containerStopwatch: {
-    alignItems: "center",
+    alignItems: 'center',
   },
   controllerPlay: {
     borderRadius: 50,
     backgroundColor: '#D9D9D9',
     paddingHorizontal: 15,
-    paddingVertical: 5
+    paddingVertical: 5,
   },
 });
